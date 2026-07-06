c6f1efd3a199949eecb552747b5d2b3808fb9c9a
arjun.rajkumar <arjunrajkumars@gmail.com>
2026-07-06 13:48:00 +0530

    Move Xero invoice guard to before action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,17 @@
 class InvoicesController < ApplicationController
-  def index
-    @xero_integration = Current.account.accounting_integrations.xero.connected.first
-    return redirect_to new_xero_connection_path unless @xero_integration
+  before_action :set_xero_integration
 
+  def index
     set_page_and_extract_portion_from Current.account.invoices.includes(:accounting_integration).recent
+  end
+
+  private
+
+  def set_xero_integration
+    if xero_integration = Current.account.accounting_integrations.xero.connected.first
+      @xero_integration = xero_integration
+    else
+      redirect_to new_xero_connection_path
+    end
   end
 end
